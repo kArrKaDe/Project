8de6ac4a516a975cd34f636f71aafa8c16631df1
kArrKaDe <maxon130309@gmail.com>
2024-02-13 21:20:22 +0500

    gg

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public Transform CameraAxisTransform;
+
+    public float RotationSpeed;
+    public float minAngle;
+    public float maxAngle;
+
+    void Update()
+    {
+        RotationY();
+        RotationX();
+    }
+
+
+
+    void RotationY()
+    {
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
+
+
+    }
+    void RotationX()
+    { 
+        var newAngleX = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y");
+        if (newAngleX > 180)
+            newAngleX -= 360;
+        newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
+        
+        CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gravity = 9.8f;
+
+    public float jumpForce;
+    public float maxJumpForce;
+    public float minJumpForce;
+
+    public float minSpeed;
+    public float maxSpeed;
+    public float speed;
+
+    private float _fallVelocity = 0;
+
+    private CharacterController _characterController;
+
+    private Vector3 _moveVector;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
+
+        if (_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
+
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.deltaTime); 
+    }
+
+    void Update()
+    {
+        Jump();
+
+        Movement();
+
+        Sprint();
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            Debug.Log("Jump");
+            _fallVelocity = -jumpForce;
+        }
+    }
+
+    void Movement()
+    {
+        _moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            _moveVector -= transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+    }
+
+    void Sprint()
+    {
+        jumpForce = minJumpForce;
+        speed = minSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            jumpForce = maxJumpForce;
+            speed = maxSpeed;
+        }
+    }
+}
